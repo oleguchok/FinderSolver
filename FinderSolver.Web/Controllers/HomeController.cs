c0a68c72c03fac4c8a924c44ecc608cde9967868
oleguchok <oleguchok@gmail.com>
2017-01-18 22:09:20 +0300

    init mvc5 web template

---
+++ FinderSolver.Web/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace FinderSolver.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
