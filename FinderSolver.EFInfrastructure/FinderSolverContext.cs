9f373552d6bb06e6f71ebce9882e02e466a71909
oleguchok <oleguchok@gmail.com>
2017-01-18 23:31:17 +0300

    Add ORM (EF) Infrastructure. Add migrations

---
+++ FinderSolver.EFInfrastructure/FinderSolverContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using FinderSolver.Core.Entities;
+
+namespace FinderSolver.EFInfrastructure
+{
+    public class FinderSolverContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
