00617aa4b73335bb03d73f6d066549e0c72dca7e
oleguchok <oleguchok@gmail.com>
2017-01-18 22:18:31 +0300

    Add Domain (core) layer

---
+++ FinderSolver.Core/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FinderSolver.Core.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; } // todo: Move to enum?
+    }
+}
