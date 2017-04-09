3391a09b83afa4f2df20ef2b62df1729f42039c5
oleguchok <oleguchok@gmail.com>
2017-04-09 21:33:04 +0300

    Added api proj

---
+++ FinderSolver.API/FinderSolver.API/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+using FinderSolver.API.Models.Dto;
+
+namespace FinderSolver.API.Controllers
+{
+    public class AccountController : ApiController
+    {
+        public IHttpActionResult Login(LoginDto loginDto)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ FinderSolver.API/FinderSolver.API/Models/Dto/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FinderSolver.API.Models.Dto
+{
+    public class LoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
